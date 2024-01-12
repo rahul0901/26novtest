98e41e93e70b7186107650872fb416338ced71e6
rahul0901 <rahul.shinde0904@gmail.com>
2024-01-13 01:02:47 +0530

    addd

--- 26novtest/src/Components/DisplayGrid.jsx
+++ 26novtest/src/Components/DisplayGrid.jsx
@@ -2,19 +2,22 @@ import React, { useState } from 'react';
 
 
 const DisplayGrid = () => {
-    const grid = [1,2,3,4,5,6,7,8,9]
-  return (
-    <>
-        <div>Display Grid View</div>
-        <div className='parent-grid'>
-            {grid.map((item, index)=>(
-                <div key={index}>
-                    <div>{item}</div>
-                </div>
-            ))}
-        </div>
-    </>
-  )
+    const grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    return (
+        <>
+            <div>Display Grid View</div>
+            <div className='parent-grid'>
+                {grid.map((item, index) => (
+                    <div key={index}>
+                        <div>{item}</div>
+                    </div>
+                ))}
+            </div>
+
+            <a style={{color:"red"}}>find job</a><i class="fa-icon-search" style={{color:"red"}}></i>
+            <p>find jobbb<i class="fa-solid fa-magnifying-glass"></i></p>
+        </>
+    )
 }
 
 export default DisplayGrid;
